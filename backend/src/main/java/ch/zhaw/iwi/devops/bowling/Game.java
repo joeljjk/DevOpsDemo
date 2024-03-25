75b0ed853bfe45b93214cbaca997bd5622ca9ff0
Kurmajoe <kurmajoe@students.zhaw.ch>
2024-03-25 13:23:24 +0100

    create game cases

--- backend/src/main/java/ch/zhaw/iwi/devops/bowling/Game.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/bowling/Game.java
@@ -2,9 +2,48 @@ package ch.zhaw.iwi.devops.bowling;
 
 
 public class Game {
-
-	public static void main(String[] args) {
-        System.out.println("Hello world");
-	}
-
+    private int[] rolls = new int[21]; // 21 is the max number of rolls in a game
+    private int currentRoll = 0;
+    
+    public void roll(int pins) {
+        rolls[currentRoll++] = pins;
+    }
+    
+    public int score() {
+        int score = 0;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(frameIndex)) { // Strike
+                score += 10 + strikeBonus(frameIndex);
+                frameIndex++;
+            } else if (isSpare(frameIndex)) { // Spare
+                score += 10 + spareBonus(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += sumOfBallsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        }
+        return score;
+    }
+    
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
+    }
+    
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
+    
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+    
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex + 2];
+    }
+    
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
 }
